c7e5c4c2c5d3056c1907ed6ab92190bc9f05bb6b
Braden Everson <bradeneverson@gmail.com>
2024-07-04 11:38:52 -0500

    Convert 32 bit floats to 64

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,30 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize)]
 pub struct LakeData{
     coords: Coords,
-    pull: f32,
+    pull: f64,
     label: Option<bool>
 }
 
 impl Default for LakeData {
     fn default() -> Self {
-        Self { coords: Coords::default(), pull: 0f32, label: None }
+        Self { coords: Coords::default(), pull: 0f64, label: None }
     }
 }
 
 impl Default for Coords {
     fn default() -> Self {
-        Self { lon: 0f32, lat: 0f32 }
+        Self { lon: 0f64, lat: 0f64 }
     }
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct Coords {
-    lon: f32,
-    lat: f32
+    lon: f64,
+    lat: f64
+}
+
+impl Coords {
+    pub fn export(&self) -> (f64, f64) {
+        (self.lon, self.lat)
+    }
 }
